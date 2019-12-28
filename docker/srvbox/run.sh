1f66898e59cd8405d40effafd75714cb9a4db861
Nathan Genetzky <nathan@genetzky.us>
2019-12-28 01:25:34 -0600

    srvbox: Add some python tools and git-annex

--- docker/srvbox/run.sh
+++ docker/srvbox/run.sh
@@ -4,5 +4,8 @@ D_SCRIPT="$(CDPATH='' cd -- "$(dirname -- "$0")" && pwd -P)"
 NAME="$(basename "${D_SCRIPT}")"
 
 docker run -d --rm  \
-    -p '10022:22' \
+    -p '10122:22' \
+    -p '10180:8888' \
+    -v '/:/mnt/:ro' \
+    -v "/data/ngenetzky/workspaces/notebook:/home/ngenetzky/workspace/" \
     "${NAME}"
