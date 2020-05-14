38470d9a12a699ca95458a6391d9c2d33d828601
Nathan Genetzky <nathan@genetzky.us>
2020-05-14 00:21:11 -0500

    ansible: Add code to generate cookiecutter/0/

---
+++ ansible/cookiecutter/0/generate.bash
@@ -0,0 +1,26 @@
+#!/bin/bash
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+    # Bash Strict Mode
+    set -eu -o pipefail
+    IFS=$'\n\t'
+fi
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+TEMPLATE='cookiecutter-ansible-playbook'
+readonly \
+    SCRIPT_DIR \
+    TEMPLATE
+
+main(){
+    cd "${SCRIPT_DIR}"
+
+    cookiecutter --verbose --no-input \
+        --debug-file "debug.log" \
+        --config-file "cookiecutter.yml" \
+        --output-dir "templates" \
+        "${TEMPLATE}"
+}
+
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+    main "$@"
+fi
