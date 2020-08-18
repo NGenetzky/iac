0f8eec79a1165b8f275cef4a65d3d15b1695bd04
Nathan Genetzky <nathan@genetzky.us>
2020-08-18 00:21:02 -0500

    docker/phusion: Allow programatic user creation

--- docker/phusion-baseimage/rootfs/sbin/entrypoint.sh
+++ docker/phusion-baseimage/rootfs/sbin/entrypoint.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 set -eu
 
-PHUSION_USER="${PHUSION_USER-user}"
+BUILDER_USER="${BUILDER_USER-user}"
 
 if [ $# -eq 0 ]; then
-    /sbin/setuser "${PHUSION_USER}" \
+    /sbin/setuser "${BUILDER_USER}" \
         bash -l
 elif [ -f "/etc/entrypoint.d/$1" ]; then
     # Intentionally not calling 'shift'
-    /sbin/setuser "${PHUSION_USER}" \
+    /sbin/setuser "${BUILDER_USER}" \
         "/etc/entrypoint.d/$1" \
         "$@"
 else
-    /sbin/setuser "${PHUSION_USER}" \
+    /sbin/setuser "${BUILDER_USER}" \
         "$@"
 fi
 exit $?
