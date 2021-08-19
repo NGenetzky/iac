604df3dc586f9162756087d20ee79dfc51011525
Nathan Genetzky <nathan@genetzky.us>
2021-08-19 01:05:19 -0500

    debops: Upgrade and switch to installing in user site-packages

--- ansible/scripts/install_debops.sh
+++ ansible/scripts/install_debops.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 sudo apt install build-essential python3-virtualenv virtualenv python3-dev \
 	                 libffi-dev libssl-dev libsasl2-dev libldap2-dev python3-pip
-pip3 install debops[ansible]==1.2.0
+pip3 install --user debops[ansible]==2.2.1
