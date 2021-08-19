045790d8ec6e1377b3102f30ab71024a06675b2d
Nathan Genetzky <nathan@genetzky.us>
2021-08-18 21:29:40 -0500

    xonsh: wip

---
+++ docker/phusion-baseimage/xonsh_setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#pip3 install --user xonsh-apt-tabcomplete xontrib-argcomplete xontrib-fzf-widgets xontrib-powerline xontrib-prompt-vi-mode xontrib-sh
+#pip3 install prompt_toolkit jedi
+
+pip3 install xonsh-apt-tabcomplete xontrib-argcomplete xontrib-fzf-widgets xontrib-prompt-vi-mode xontrib-sh
