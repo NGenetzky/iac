8cee110207384f84c12e8054217d1fa0b688c665
Nathan Genetzky <nathan@genetzky.us>
2022-01-08 22:25:36 -0600

    cloud-init: ubuntu-armhf: Allow CONFIGDIR to be customized

--- cloud-init/ubuntu_system_boot_config.bash
+++ cloud-init/ubuntu_system_boot_config.bash
@@ -2,11 +2,11 @@
 echoerr(){ echo "$*" >&2 ; }
 die(){ echo "$*" >&2 ; exit 1 ; }
 
-BOOTDIR="${1?Required argument}"
-
 D_SCRIPT="$(CDPATH='' cd -- "$(dirname -- "$0")" && pwd -P)"
-CONFIGDIR="${D_SCRIPT}/ubuntu-20.04-preinstalled-server-arm64/"
+_DEFAULT_CONFIGDIR="${D_SCRIPT}/ubuntu-20.04-preinstalled-server-arm64/"
 
+BOOTDIR="${1?Required argument}"
+CONFIGDIR="${2-"${_DEFAULT_CONFIGDIR}"}"
 
 cd "${CONFIGDIR}" || die "failed to cd to CONFIGDIR"
 cp -t "${BOOTDIR}" \
--- cloud-init/ubuntu_system_boot_config_rpi_zero_2.bash
+++ cloud-init/ubuntu_system_boot_config_rpi_zero_2.bash
@@ -2,10 +2,11 @@
 echoerr(){ echo "$*" >&2 ; }
 die(){ echo "$*" >&2 ; exit 1 ; }
 
-BOOTDIR="${1?Required argument}"
-
 D_SCRIPT="$(CDPATH='' cd -- "$(dirname -- "$0")" && pwd -P)"
-CONFIGDIR="${D_SCRIPT}/ubuntu-20.04-preinstalled-server-arm64/"
+_DEFAULT_CONFIGDIR="${D_SCRIPT}/ubuntu-20.04-preinstalled-server-arm64/"
+
+BOOTDIR="${1?Required argument}"
+CONFIGDIR="${2-"${_DEFAULT_CONFIGDIR}"}"
 
 cd "${CONFIGDIR}" || die "failed to cd to CONFIGDIR"
 cp -t "${BOOTDIR}" \
@@ -23,7 +24,7 @@ cp -t "${BOOTDIR}" \
 # - https://waldorf.waveform.org.uk/2021/the-pi-zero-2.html
 # - https://waldorf.waveform.org.uk/2021/you-boot-no-u-boot-first.html
 
+# skip u-boot
+cp "config-rpi-zero-2.txt" "${BOOTDIR}/config.txt"
 # reuse dtb for rpi 3b for rpi zero 2
 cp "${BOOTDIR}/bcm2710-rpi-3-b.dtb" "${BOOTDIR}/bcm2710-rpi-zero-2.dtb"
-# skip u-boot
-cp "${CONFIGDIR}/config-rpi-zero-2.txt" "${BOOTDIR}/config.txt"
