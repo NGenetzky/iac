84700ca297e5b3a27f20b2fe920528c3ddb3abdb
Nathan Genetzky <nathan@genetzky.us>
2024-02-25 00:05:26 -0600

    fixup! ansible: Note how to install

--- ansible/scripts/setup-ansible.bash
+++ ansible/scripts/setup-ansible.bash
@@ -1,4 +1,20 @@
 #!/bin/bash
 # Opt 1: Use pipx
-# pipx install ansible
 # Opt 2: https://www.buildahomelab.com/2022/04/26/how-to-install-ansible-with-pipenv-pyenv/
+
+# TODO: Do I need *both*?
+# pipx install ansible
+# pipx install ansible-core
+#   installed package ansible-core 2.16.3, installed using Python 3.10.12
+#   These apps are now globally available
+#     - ansible
+#     - ansible-config
+#     - ansible-connection
+#     - ansible-console
+#     - ansible-doc
+#     - ansible-galaxy
+#     - ansible-inventory
+#     - ansible-playbook
+#     - ansible-pull
+#     - ansible-test
+#     - ansible-vault
